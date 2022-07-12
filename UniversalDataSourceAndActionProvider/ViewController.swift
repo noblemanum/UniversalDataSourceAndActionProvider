b15e1f5cdfbc56ed16b27410128d330620420ac4
Сницар Д.А <da.snicar@tensor.ru>
2022-07-12 13:22:14 +0300

    Initial commit

---
+++ UniversalDataSourceAndActionProvider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniversalDataSourceAndActionProvider
+//
+//  Created by Сницар Д.А. on 12.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
